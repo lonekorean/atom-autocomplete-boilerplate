3e6ce2aadd938d742162b07a073872ff854cea77
Will Boyd <will@codersblock.com>
2017-04-14 19:10:58 -0400

    Sample files

---
+++ samples/file.php
@@ -0,0 +1,6 @@
+<?php
+	$var1 = 'This is a simple PHP file. Only the basic provider will work here.';
+	$var2 = 'Go ahead, start typing the name of a fruit (basic).';
+	$var3 = 'Then try typing the word "super" (intermediate) or "@" (advanced).';
+	$var4 = 'Notice they won\'t work here.';
+?>
---
+++ samples/file.txt
@@ -0,0 +1,4 @@
+This is a simple text file. All 3 providers will work here.
+
+Go ahead, start typing the name of a fruit (basic provider), the word "super"
+(intermediate provider), or "@" (advanced provider).
